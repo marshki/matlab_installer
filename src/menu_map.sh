e9650b8a8ab932ea6c548f737f26fe55fb3cc9ea
marshki <marshall@morethantomorrow.org>
2018-01-30 13:14:27 -0500

    preliminaries

---
+++ src/menu_map.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+# Map Matlab version to download URL 
+
+MATLAB_8.5_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab8.5.tgz"
+MATLAB_8.6_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab8.6.tgz"
+MATLAB_9.0_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.0.tgz"
+MATLAB_9.1_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.1.tgz"
+MATLAB_9.2_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.2.tgz"
+MATLAB_9.3_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.3.tgz"
