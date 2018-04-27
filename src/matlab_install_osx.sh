0be48181c0c7601a40e9296b4be2672b78a1444e
marshki <marshki@users.noreply.github.com>
2018-04-26 20:36:13 -0400

    fix ping function spacing

--- src/matlab_install_osx.sh
+++ src/matlab_install_osx.sh
@@ -35,12 +35,12 @@ fi
 # Is local web pingable? If not, exit. 
 
 function ping_local_web () {
-printf "%s\n" "Pinging local web..."
-if ping -c 1 "$LOCAL_WEB" &> /dev/null; then
-  printf "%s\n" "1; reachable. Continuing..."
-else
-  printf "%s\n" "0; unreachable. Exiting." >&2
-  exit 1
+  printf "%s\n" "Pinging local web..."
+  if ping -c 1 "$LOCAL_WEB" &> /dev/null; then
+    printf "%s\n" "1; reachable. Continuing..."
+  else
+    printf "%s\n" "0; unreachable. Exiting." >&2
+    exit 1
 fi
 }
 
