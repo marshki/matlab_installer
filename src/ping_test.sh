1131755c84c0d19f760e885c8621b69d0b10293c
marshki <marshall@morethantomorrow.org>
2018-04-28 15:14:19 -0400

    change print statements

--- src/ping_test.sh
+++ src/ping_test.sh
@@ -8,12 +8,12 @@ LOCAL_WEB="128.122.112.23"
 
 function ping_local_web () {
 
-printf "%s\n" "Pinging local web..."
+printf "%s\n" "Pinging CNS local web..."
 
 if ping -c 1 "$LOCAL_WEB" &> /dev/null; then
-  printf "%s\n" "1; reachable. Continuing..."
+  printf "%s\n" "CNS local web IS reachable. Continuing..."
 else
-  printf "%s\n" "0; unreachable. Exiting." >&2
+  printf "%s\n" "CNS local web IS NOT reachable. Exiting." >&2
   exit 1
 fi
 }
