f7be12b870820fbdc2a88d447517a3d3370f321d
marshki <marshall@morethantomorrow.org>
2018-05-15 11:55:23 -0400

    add array calls

--- src/matlab_install_osx_2.sh
+++ src/matlab_install_osx_2.sh
@@ -16,6 +16,10 @@ Matlab9.4
 MATLAB9.4.app
 )
 
+#######################
+#### Sanity Checks ####
+#######################
+
 # Is current UID 0? If not, exit.
 
 root_check () {
@@ -56,18 +60,29 @@ ping_local_web() {
 fi
 }
 
+sanity_checks() {
+  root_check
+  check_disk_space
+  curl_check
+  ping_local_web
+}
+
+##########################
+#### Matlab Install-r ####
+##########################
+
 # Download tarball to /Applications
 
 get_matlab () {
-  printf "%s\n" "Retrieving Matlab insaller..."
+  printf "%s\n" "Retrieving ${MATLAB[0]} insaller..."
   
-  curl --progress-bar --retry 3 --retry-delay 5 "$MATLAB_INSTALLER" --output /Applications/matlab.app.tgz
+  curl --progress-bar --retry 3 --retry-delay 5 "${MATLAB[1]}" --output /Applications/matlab.app.tgz
 }
 
 # Unpack tarball to /Applications.
 
 untar_matlab () {
-  printf "%s\n" "Untarring package to /Applications..."
+  printf "%s\n" "Untarring ${MATLAB[0]} package to /Applications..."
   
   tar --extract --gzip -v --file=/Applications/matlab.app.tgz --directory=/Applications
 }
@@ -75,7 +90,7 @@ untar_matlab () {
 # Remove tarball from /Applications.
 
 remove_matlab_tar () {
-  printf "%s\n" "Removing Matlab tarball..."
+  printf "%s\n" "Removing ${MATLAB[0]} tarball..."
 
   rm -rv /Applications/matlab.app.tgz
 }
@@ -92,15 +107,19 @@ fi
 # Create symlink for Matlab
 
 symlink_matlab () {
-  printf "%s\n" "Creating symlink..."
+  printf "%s\n" "Creating symlink for ${MATLAB[0]}..."
 
   ln -s /Applications/MATLAB9.4.app/bin/matlab /usr/local/bin/matlab
 }
 
+###################
+#### Launch-r ####
+###################
+
 # Launch Matlab from terminal
 
 launch_matlab () {
-  printf "%s\n" "Launching Matlab..."
+  printf "%s\n" "Launching ${MATLAB[0]}..."
 
   matlab -nodesktop
 }
@@ -108,16 +127,13 @@ launch_matlab () {
 # Main 
 
 main () {
-	root_check
-	check_disk_space
-	curl_check
-  	ping_local_web
-  	get_matlab
-	untar_matlab
-	remove_matlab_tar
-  	local_bin_check
-	symlink_matlab
-	launch_matlab
+  sanity_checks
+  get_matlab
+  untar_matlab
+  remove_matlab_tar
+  local_bin_check
+  symlink_matlab
+  launch_matlab
 }
 
 main "$@"
