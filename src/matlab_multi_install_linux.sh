27ea1ef38113c779592cd70f576a281992dca5dd
marshki <marshall@morethantomorrow.org>
2018-05-17 11:29:34 -0400

    added urls for arrays

--- src/matlab_multi_install_linux.sh
+++ src/matlab_multi_install_linux.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+"http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab8.5.tgz"
+"http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab8.6.tgz"
+"http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.0.tgz"
+"http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.1.tgz"
+"http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.2.tgz"
+"http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.3.tgz"
+"http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux/matlab9.4.tgz"
+
