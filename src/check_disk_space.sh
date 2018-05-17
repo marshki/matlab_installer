d71ce24e3b6f27d63ed363ef5ed1812c91b2c1d0
marshki <marshall@morethantomorrow.org>
2018-05-17 12:04:09 -0400

    # as a reminder

--- src/check_disk_space.sh
+++ src/check_disk_space.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Works in Ubuntu but not Raspbian. Need to test...
+
 check_disk_space () {
   if [ $(df -Hl --output=avail /usr/local |awk 'FNR == 2 {print $1}' |sed 's/G//') -le "30" ]; then
     printf "%s\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
