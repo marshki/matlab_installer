740313b86aef73722f8c68b8b6b34d160352db66
marshki <marshall@morethantomorrow.org>
2018-05-17 11:54:17 -0400

    check function in raspbian os

---
+++ src/check_disk_space.sh
@@ -0,0 +1,8 @@
+check_disk_space () {
+  if [ $(df -Hl --output=avail /usr/local |awk 'FNR == 2 {print $1}' |sed 's/G//') -le "30" ]; then
+    printf "%s\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
+    exit 1
+fi
+}
+
+check_disk_space
