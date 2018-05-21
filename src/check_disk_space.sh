c9e88a08a2836872c904a8b756c596670625ea25
marshki <marshall@morethantomorrow.org>
2018-05-21 19:16:14 -0400

    add kb check to disk check function

--- src/check_disk_space.sh
+++ src/check_disk_space.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Works in Ubuntu but not Raspbian. Need to test...
-
+# In circumstances where df -Hl outputs a decimal we will get an "integer expression expected error", but the program won't exit
+# As a precaution, let's go to kbs 
+ 
 check_disk_space () {
-  if [ $(df -Hl --output=avail /usr/local |awk 'FNR == 2 {print $1}' |sed 's/G//') -le "30" ]; then
+  if [ $(df -l --output=avail /usr/local |awk 'FNR == 2 {print $1}' |sed 's/G//') -le "31457280" ]; then
     printf "%s\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
     exit 1
 fi
 }
+
+#check_disk_space () {
+#  if [ $(df -Hl --output=avail /usr/local |awk 'FNR == 2 {print $1}' |sed 's/G//') -le "30" ]; then
+#    printf "%s\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
+#    exit 1
+#fi
+#}
 
 check_disk_space
