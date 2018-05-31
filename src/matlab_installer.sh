61a5eea26d496275ca2bf895acc943d96f882788
marshki <marshki@users.noreply.github.com>
2018-05-31 18:40:09 -0400

    Add files via upload

--- src/matlab_installer.sh
+++ src/matlab_installer.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # mjk235 [at] nyu [dot] edu --2017.02.10
-# v.0.2
+# v.0.2 
 
 #########################################################################################
-#### Auto install latest version of Matlab on Linux (Debian-based).                  ####
-#### Open to members of NYU's Center for Neural Science and Department of Psychology ####
-#### Requires: root privileges; access to Meyer network; adequate free disk space.   ####
+#### Install latest version of Matlab on Linux (Debian-based). 		             ####
+#### Open to members of NYU's Center for Neural Science and Department of Psychology #### 
+#### Requires: root privileges; access to Meyer network; adequate free disk space.   ####   
 #### Note: Use on machines WITHOUT previous version of MATLAB installed on them.     ####
 #########################################################################################
-
-# dialog_box="dialog --backtitle "$script" --title "$program" --msgbox"
-# dialog_dimension"10 40" 
 
 script=`basename "$0"`
 program="MATLAB INSTALLER"
@@ -33,8 +30,8 @@ matlab9.4
 
 progress_meter () {
   for ((i=1;i<=PCT;i++)); do
-  echo $i;
-  sleep 0.1;
+  echo $i; 
+  sleep 0.1; 
   done
 }
 
@@ -54,11 +51,11 @@ fi
 # Is there adequate disk space in /usr/local directory? If not, exit.
 
 check_disk_space () {
-  if [ $(df -l --output=avail /usr/local |awk 'FNR == 2 {print $1}') -le "3" ]; then #"31457280"
+  if [ $(df -l --output=avail /usr/local |awk 'FNR == 2 {print $1}') -le "31457280" ]; then 
     dialog --backtitle "$script" --title "$program" --msgbox "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2 10 40
     exit 1
 fi
-} 
+}
 
 # Is curl installed? If not, install it.
 
@@ -69,14 +66,14 @@ curl_check () {
 fi
 }
 
-# Is CNS local web available? If not, exit.
+# Is CNS local web available? If not, exit. 
 
 ping_local_web () {
-
-  progress_meter | dialog --backtitle "$script" --title "$program" --gauge "PINGING CNS LOCAL WEB..." 10 40
+ 
+  progress_meter | dialog --backtitle "$script" --title "$program" --gauge "PINGING CNS LOCAL WEB..." 10 40 
 
   if ping -c 1 "$LOCAL_WEB" &> /dev/null; then
-    dialog --backtitle "$script" --title "$program" --msgbox "CNS LOCAL WEB IS REACHABLE. CONTINUING..." 10 40
+    dialog --backtitle "$script" --title "$program" --msgbox "CNS LOCAL WEB IS REACHABLE. CONTINUING..." 10 40 
   else
     dialog --backtitle "$script" --title "$program" --msgbox "ERROR: CNS LOCAL WEB IS NOT REACHABLE. EXITING." >&2 10 40
     exit 1
@@ -84,17 +81,17 @@ fi
 }
 
 sanity_checks () {
-  root_check
-  check_disk_space
-  curl_check
+  root_check 
+  #check_disk_space
+  curl_check 
   ping_local_web
-}
+} 
 
 ##########################
 #### Matlab Install-r ####
 ##########################
 
-# Download tarball to /usr/local.
+# Download tarball to /usr/local. 
 
 get_matlab () {
   dialog --backtitle "$script" --title "$program" --infobox "RETRIEVING ${MATLAB[0]} INSTALLER..." 10 40
@@ -102,7 +99,7 @@ get_matlab () {
   curl --progress-bar --retry 3 --retry-delay 5 "${MATLAB[1]}" --output /usr/local/matlab.tgz
 }
 
-# Unpack tarball to /usr/local which installs Matlab
+# Unpack tarball to /usr/local which installs Matlab 
 
 untar_matlab () {
   dialog --backtitle "$script" --title "$program" --infobox "UNTARRING ${MATLAB[0]} PACKAGE TO /usr/local..." 10 40
@@ -110,59 +107,53 @@ untar_matlab () {
   tar --extract --gzip --verbose --file=/usr/local/matlab.tgz --directory=/usr/local
 }
 
-# Remove tarball.
+# Remove tarball. 
 
 remove_matlab_tar () {
-  dialog --backtitle "$script" --title "$program" --infobox "REMOVING $MATLAB[0]} INSTALLER..." 10 40
-
+  dialog --backtitle "$script" --title "$program" --infobox "REMOVING $MATLAB[0]} INSTALLER..." 10 40 ; sleep 2 
+ 
   rm --recursive --verbose /usr/local/matlab.tgz
 }
 
-# Does /usr/local/bin exist? If not, add it.
+# Does /usr/local/bin exist? If not, add it. 
 
 local_bin_check () {
   if [ ! -d "/usr/local/bin" ] ; then
 
-    dialog --backtitle "$script" --title "$program" --infobox "/usr/local/bin DOES NOT exist; LET'S ADD IT..." 10 40
-
+    dialog --backtitle "$script" --title "$program" --infobox "/usr/local/bin DOES NOT exist; LET'S ADD IT..." 10 40 ; sleep 2 
+    
     mkdir -pv /usr/local/bin
 fi
 }
 
-# Create symbolic link for Matlab.
+# Create symbolic link for Matlab. 
 
 symlink_matlab () {
-  dialog --backtitle "$script" --title "$program" --infobox "CREATING SYMLINK FOR ${MATLAB[0]}..." 10 40
+  dialog --backtitle "$script" --title "$program" --infobox "CREATING SYMLINK FOR ${MATLAB[0]}..." 10 40 ; sleep 2 
 
   ln --symbolic /usr/local/${MATLAB[2]}/bin/matlab /usr/local/bin/matlab
 }
 
+# Install complete message.  
+
+install_complete () {
+   dialog --backtitle "$script" --title "$program" --msgbox "${MATLAB[0]} installed successfully!" 10 40
+}
+
 matlab_installer () {
-  get_matlab
+  get_matlab 
   untar_matlab
   remove_matlab_tar
   local_bin_check
   symlink_matlab
-}
+  install_complete
+} 
 
-###################
-#### Launch-r ####
-###################
-
-# Launch Matlab from terminal.
-
-launch_matlab () {
-  printf "%s\n" "LAUNCHING ${MATLAB[0]}..."
-
-  matlab -nodesktop
-}
-
-# Main
+# Main 
 
 main () {
   sanity_checks
   matlab_installer
-  #launch_matlab
 }
 
 main "$@"
