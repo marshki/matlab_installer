1e4a3f1b83d926f98b99d87190faaccb564e3874
marshki <marshall@morethantomorrow.org>
2018-06-04 10:37:05 -0400

    even more testing

--- src/wget_progress_bar.sh
+++ src/wget_progress_bar.sh
@@ -4,16 +4,20 @@ MATLAB_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux
 
 # Download tarball to /Applications. 
 
-get_matlab () {
-  printf "%s\n" "RETRIEVING MATLAB INSTALLER..."
+#get_matlab () {
 
-  wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | 
-    grep --line-buffered "%" | \
-    sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}' | 
+#  wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | 
+#    grep --line-buffered "%" | \
+#    sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}' | 
+#    dialog --gauge "DAT MATLAB DOE..." 10 40 
+
+get_matlab () { 
+  wget "$MATLAB_INSTALLER" 2>&1 | \
+    stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
     dialog --gauge "DAT MATLAB DOE..." 10 40 
+} 
 
 get_matlab 
-
 
 #  wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | \
 #    grep "%" |\
