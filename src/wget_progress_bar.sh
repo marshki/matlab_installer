f87ed2de549d70e0a4ba372079f68e5ab58f6132
marshki <marshall@morethantomorrow.org>
2018-06-04 09:58:46 -0400

    more testing for wget progress bar

--- src/wget_progress_bar.sh
+++ src/wget_progress_bar.sh
@@ -7,9 +7,12 @@ MATLAB_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux
 get_matlab () {
   printf "%s\n" "RETRIEVING MATLAB INSTALLER..."
 
-  wget --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | \
-    stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
+  wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | \
+    grep "%" |\
+    sed -u -e "s,\.,,g" | awk '{print $2}' | sed -u -e "s,\%,,g" |
     dialog --gauge "Dat Matlab doe..." 10 100
 }
 
 get_matlab
+
+#stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
