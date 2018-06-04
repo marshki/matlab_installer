b1317562dd548d0be1111719b46534d73205dc08
marshki <marshall@morethantomorrow.org>
2018-06-04 11:23:46 -0400

    wget progress fix?

--- src/wget_progress_bar.sh
+++ src/wget_progress_bar.sh
@@ -4,24 +4,11 @@ MATLAB_INSTALLER="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/linux
 
 # Download tarball to /Applications. 
 
-#get_matlab () {
-
-#  wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | 
-#    grep --line-buffered "%" | \
-#    sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}' | 
-#    dialog --gauge "DAT MATLAB DOE..." 10 40 
-
-get_matlab () { 
-  wget "$MATLAB_INSTALLER" 2>&1 | \
-    stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
-    dialog --gauge "DAT MATLAB DOE..." 10 40 
-} 
+get_matlab () {
+  wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | \
+    grep "%" |\
+    sed -u -e "s,\.,,g" | awk '{print $2}' | sed -u -e "s,\%,,g" |
+    dialog --gauge "Dat Matlab doe..." 10 100
+}
 
 get_matlab 
-
-#  wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue $MATLAB_INSTALLER 2>&1 | \
-#    grep "%" |\
-#    sed -u -e "s,\.,,g" | awk '{print $2}' | sed -u -e "s,\%,,g" |
-#    dialog --gauge "Dat Matlab doe..." 10 100
-#}
-#stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
