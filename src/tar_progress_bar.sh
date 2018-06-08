66b1b0099e7c5b4e22b3e420d9f1e74cfc329056
marshki <marshall@morethantomorrow.org>
2018-06-08 12:17:13 -0400

    update

--- src/tar_progress_bar.sh
+++ src/tar_progress_bar.sh
@@ -1,10 +1,20 @@
 #!/bin/bash 
-# Progress bar using tar, pv, and dialog in Linux. 
+# A "dialog" progress bar using tar and pv. 
 
-untar_matlab () {
+# Install pre-reqs: 
+# sudo apt-get --yes install dialog pv   
+
+# Create a file (optional, but done here for illustrative purposes): 
+# sudo fallocate --length 2GB /usr/local/rando.img
+
+# Compress file (optional, but done here for illustrative purposes): 
+# tar -cvzf /usr/local/rando.tgz /usr/local/rando.img 
+# 
+
+
+untar_package () {
   (pv --numeric /usr/local/rando.tgz | tar --extract --gzip --directory=/usr/local) 2>&1|
-  dialog --backtitle "$script" --title "bleh" --gauge "UNTARRING rando PACKAGE TO /usr/local..." 10 40
+  dialog --backtitle "$script" --title "bleh" --gauge "UNTARRING PACKAGE TO /usr/local..." 10 40
 }
 
-untar_matlab
-
+untar_package
