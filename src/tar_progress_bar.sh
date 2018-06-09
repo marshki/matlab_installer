83c4561b467de0a933a5c5efb66bb0063dcc168d
marshki <marshall@morethantomorrow.org>
2018-06-08 20:43:55 -0400

    verbose commands

--- src/tar_progress_bar.sh
+++ src/tar_progress_bar.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
 # A "dialog" progress bar using tar and pv. 
 
+script=`basename "$0"`
+program="Tar & Feather" 
+
+
 # Install pre-reqs: 
 # sudo apt-get --yes install dialog pv   
 
@@ -8,13 +12,11 @@
 # sudo fallocate --length 2GB /usr/local/rando.img
 
 # Compress file (optional, but done here for illustrative purposes): 
-# tar -cvzf /usr/local/rando.tgz /usr/local/rando.img 
-# 
-
+# tar --create --verbose --gzip --file=/usr/local/rando.tgz /usr/local/rando.img 
 
 untar_package () {
   (pv --numeric /usr/local/rando.tgz | tar --extract --gzip --directory=/usr/local) 2>&1|
-  dialog --backtitle "$script" --title "bleh" --gauge "UNTARRING PACKAGE TO /usr/local..." 10 40
+  dialog --backtitle "$script" --title "$program" --gauge "UNTARRING PACKAGE TO /usr/local..." 10 40
 }
 
 untar_package
