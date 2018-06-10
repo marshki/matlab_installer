93b9b16153a8f75a50145b4120b2740158444c17
marshki <marshki@users.noreply.github.com>
2018-06-10 12:23:45 -0400

    Update tar_progress_bar.sh

--- src/tar_progress_bar.sh
+++ src/tar_progress_bar.sh
@@ -1,22 +1,31 @@
-#!/bin/bash 
-# A "dialog" progress bar using tar and pv. 
+#!/bin/bash
+
+# A "dialog" progress bar for opening an archive, using tar and pv.
 
 script=`basename "$0"`
-program="Tar & Feather" 
+program="Tar & Feather"
 
+# Change dirs, then install pre-reqs:
 
-# Install pre-reqs: 
-# sudo apt-get --yes install dialog pv   
+cd /home/*/Desktop && apt-get --yes install dialog pv
 
-# Create a file (optional, but done here for illustrative purposes): 
-# sudo fallocate --length 2GB /usr/local/rando.img
+# Create file (optional, but done here for illustrative purposes): 
 
-# Compress file (optional, but done here for illustrative purposes): 
-# tar --create --verbose --gzip --file=/usr/local/rando.tgz /usr/local/rando.img 
+fallocate --length 2GB rando.img
+ 
+# Create tar archive (optional, but done here for illustrative purposes):
 
-untar_package () {
+tar --create --verbose --gzip --file=/usr/local/rando.tgz rando.img
+
+# Untar 
+
+untar_blob () {
   (pv --numeric /usr/local/rando.tgz | tar --extract --gzip --directory=/usr/local) 2>&1|
-  dialog --backtitle "$script" --title "$program" --gauge "UNTARRING PACKAGE TO /usr/local..." 10 40
+  dialog --backtitle "$script" --title "$program" --gauge "OPENING TAR ARCHIVE IN /usr/local..." 10 40
 }
 
-untar_package
+main () { 
+  untar_blob
+}
+
+main "$@"
