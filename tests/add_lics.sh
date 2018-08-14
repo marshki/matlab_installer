e1f79fead4adfb2f2fcd5cb789875743702ce395
marshki <marshall@morethantomorrow.org>
2018-08-14 10:31:11 -0400

    more succinct array contents

--- tests/add_lics.sh
+++ tests/add_lics.sh
@@ -4,20 +4,21 @@
 ################
 # MATLAB ARRAY #
 ################
+MATLAB_VERSION=( MATLAB{9.{4..0},8.{6,5,3,0},{7.5,}}.app )
 
-MATLAB_VERSION=(
-  MATLAB9.4.app
-  MATLAB9.3.app
-  MATLAB9.2.app
-  MATLAB9.1.app
-  MATLAB9.0.app
-  MATLAB8.6.app
-  MATLAB8.5.app
-  MATLAB8.3.app 
-  MATLAB8.0.app 
-  MATLAB7.5.app 
-  MATLAB.app 
-)
+#MATLAB_VERSION=(
+#  MATLAB9.4.app
+#  MATLAB9.3.app
+#  MATLAB9.2.app
+#  MATLAB9.1.app
+#  MATLAB9.0.app
+#  MATLAB8.6.app
+#  MATLAB8.5.app
+#  MATLAB8.3.app 
+#  MATLAB8.0.app 
+#  MATLAB7.5.app 
+#  MATLAB.app 
+#)
 
 ########################
 #### Sanity checks  ####
@@ -90,14 +91,12 @@ add_licks () {
     fi 
   
   done
-
 }
 
 main() { 
   root_check 
   matlab_check
-  add_licks
-  printf "%s\\n" "KAPOW! DONE." 
+  add_licks  
 }
 
 main "$@" 
