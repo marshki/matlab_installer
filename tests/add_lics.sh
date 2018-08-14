c089728572fc71b7b973695500bd22772fa42847
marshki <marshall@morethantomorrow.org>
2018-08-14 10:11:40 -0400

    fix array badness (thanks Paul!)

--- tests/add_lics.sh
+++ tests/add_lics.sh
@@ -54,9 +54,9 @@ matlab_check() {
 make_nyu_lic() { 
   # create 1NYU_NET.lic in /Applications/MATLAB*.*.app/licenses
 
-  printf "%s\\n" "ADDING 1NYU_NET.lic TO /Applications/MATLAB9.3.app/licenses" 
+  printf "%s\\n" "ADDING 1NYU_NET.lic TO /Applications/${MATLAB}/licenses" 
  
-  cat > /Applications/MATLAB9.3.app/licenses/1NYU_NET.lic << EOF
+  cat > /Applications/${MATLAB}/licenses/1NYU_NET.lic << EOF
 # NYU ITS matlab license servers - 08.01.2018
 SERVER its428-wap-v.cfs.its.nyu.edu 27000
 SERVER its429-wap-v.cfs.its.nyu.edu 27000
@@ -68,26 +68,29 @@ EOF
 make_cns_lic() { 
   # create 1CNS_NET.lic in /Applications/MATLAB*.*.app/licenses 
  
-  printf "%s\\n" "ADDING 1CNS_NET.lic to /Applications/MATLAB9.3.app/licenses" 
+  printf "%s\\n" "ADDING 1CNS_NET.lic to /Applications/${MATLAB}/licenses" 
  
-  cat > /Applications/MATLAB9.3.app/licenses/1CNS_NET.lic << EOF 
+  cat > /Applications/${MATLAB}/licenses/1CNS_NET.lic << EOF 
 # CNS license server - 08.01.2018
 SERVER matlic1.cns.nyu.edu 27000
 USE_SERVER
 EOF
 } 
 
-# pseudo code
-# add_licks () {  
-# for i in MATLAB_VERISONS; do 
-#   if /Applications/MATLAB_VERSION/licenses exits; then 
-#     "ADDING LICENSES" 
-#     make_nyu_lic
-#     make_cns_lic
-#   else  
-#     continue 
-#fi
-# } 
+add_licks () { 
+  # iterate through array and do the following:  
+  # if match --> add .lic files 
+  # if no match --> continue 
+
+  for MATLAB in "${MATLAB_VERSION[@]}"; do 
+    if [ -d "/Applications/${MATLAB}/licenses" ]; then
+      make_nyu_lic 
+      make_cns_lic 
+    else 
+      continue            
+    fi 
+  done
+}
 
 main() { 
   root_check 
