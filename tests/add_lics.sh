b271f621941844e6c5fc836aafd40b88aa703cf0
marshki <marshall@morethantomorrow.org>
2018-08-14 10:32:01 -0400

    more succinct array

--- tests/add_lics.sh
+++ tests/add_lics.sh
@@ -6,20 +6,6 @@
 ################
 MATLAB_VERSION=( MATLAB{9.{4..0},8.{6,5,3,0},{7.5,}}.app )
 
-#MATLAB_VERSION=(
-#  MATLAB9.4.app
-#  MATLAB9.3.app
-#  MATLAB9.2.app
-#  MATLAB9.1.app
-#  MATLAB9.0.app
-#  MATLAB8.6.app
-#  MATLAB8.5.app
-#  MATLAB8.3.app 
-#  MATLAB8.0.app 
-#  MATLAB7.5.app 
-#  MATLAB.app 
-#)
-
 ########################
 #### Sanity checks  ####
 ########################
