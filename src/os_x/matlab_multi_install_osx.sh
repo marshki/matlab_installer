cf5c72e9ff6e54fcf16bc067a1be471786ee381c
marshki <marshall@morethantomorrow.org>
2018-09-02 11:54:24 -0400

    double quote to prevent globbing/splitting @104

--- src/os_x/matlab_multi_install_osx.sh
+++ src/os_x/matlab_multi_install_osx.sh
@@ -101,7 +101,7 @@ fi
 # Is there adequate disk space in "/Applications"? If not, exit.
 
 check_disk_space () {
-  if [ $(df -lk /Applications |awk 'FNR == 2 {print $4}') -le "14680064" ]; then
+  if [ "$(df -lk /Applications |awk 'FNR == 2 {print $4}')" -le "14680064" ]; then
     printf "%s\\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
     exit 1
 fi
