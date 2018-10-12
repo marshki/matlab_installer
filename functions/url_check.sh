679ce642e6887c240453e412fc7fa768a9a9b0ab
marshki <marshall@morethantomorrow.org>
2018-10-12 12:36:09 -0400

    add check

--- functions/url_check.sh
+++ functions/url_check.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash 
 # check if URL is available 
 
-url="https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg"
+#url="https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg"
 #url="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz"
+#url="https://geekberg.info" 
+#url="https://google.com" 
 
-#curl -IsSf https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg > /dev/null
-#curl --head --silent --show-error --fail https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg > /dev/null
-#curl -sSf http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz > /dev/null
+url_check(){ 
+  # make this variable local 
+  curl --head --silent --show-error --fail $url > /dev/null
 
-
-url_check() { 
-
-  if [ "curl --head --silent --show-error --fail $url > /dev/null" -ne "0" ] ; then 
-    printf "%s\\n" "URL GOOD" 
-  else 
-    printf "%s\\n" "URL BAD" 
-
-  fi 
+  if [ $? -ne "0" ] ; then
+    printf "%s\\n" "BAD URL"
+  else
+    printf "%s\\n" "GOOD URL"
+fi
 } 
 
 url_check 
-
-#if curl --output /dev/null --silent --head --fail "$url"; then
-#  echo "URL exists: $url"
-#else
-#  echo "URL does not exist: $url"
-#fi
