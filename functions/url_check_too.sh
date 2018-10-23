8cebf1405829969696912ada143f972080c8b818
marshki <marshall@morethantomorrow.org>
2018-10-23 10:29:47 -0400

    improved, but not correct (yet)

--- functions/url_check_too.sh
+++ functions/url_check_too.sh
@@ -15,13 +15,18 @@
 #url="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz"
 #url="https://www.google.com" 
 #url="https://geekberg.info" 
-#url="https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg" 
+url="https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg" 
 
 wget_check() { 
   # wget to check HTTP status code; error message if it fails
-  # wget --spider --server-response http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz 2>&1 | awk '/^  HTTP/{print $2}'
+  
+  status_code=$(wget --spider --server-response $url 2>&1 | awk '/^ HTTP/{print $2}' | head -1)  
 
-
+  if [ "$status_code" -ne "200" ] ; then 
+    printf "%s\\n" "BAD URL" 
+  else 
+    printf "%s\\n" "GOOD URL" 
+fi 
 } 
 
 wget_check
