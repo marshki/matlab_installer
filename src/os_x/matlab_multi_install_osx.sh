ea519657c02d465581df271d2b06df8317e91792
marshki <marshall@morethantomorrow.org>
2018-11-10 10:57:40 -0500

    double quote status_code to avoid globbing & word-splitting

--- src/os_x/matlab_multi_install_osx.sh
+++ src/os_x/matlab_multi_install_osx.sh
@@ -126,9 +126,10 @@ fi
 # Is CNS local web available? If not, exit.
 
 local_web_check(){
-  local status_code=$(curl --output /dev/null --silent --head --write-out '%{http_code}\n' "$LOCAL_WEB")
+  local status_code
+  status_code=$(curl --output /dev/null --silent --head --write-out '%{http_code}\n' "$LOCAL_WEB")
 
-  if [ $status_code -ne "200" ] ; then
+  if [ "$status_code" -ne "200" ] ; then
     printf "%s\\n" "ERROR: CNS LOCAL WEB IS NOT REACHABLE. EXITING." >&2
     exit 1 
 
