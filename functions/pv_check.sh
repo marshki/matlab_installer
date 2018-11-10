c987e0c3a55459908d69126771f473a806d7a558
marshki <marshall@morethantomorrow.org>
2018-11-10 11:31:27 -0500

    add pv check function

---
+++ functions/pv_check.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash 
+
+# Is pv installed? If not, install it
+# add silent install so it doesn't break the dialog box
+
+pv_check () {
+  if [ "$(dpkg-query --show --showformat='${Status}' pv 2>/dev/null | grep --count "ok installed")" -eq "0" ]; then
+    dialog --backtitle "$script" --title "$program" --infobox "PV IS NOT INSTALLED. LET'S INSTALL IT..." >&2 10 40
+    apt-get install pv --yes
+fi
+}
