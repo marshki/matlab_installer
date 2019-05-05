53718e41df0b4b559192306ef31eefc925e5252e
marshki <marshall@morethantomorrow.org>
2019-05-05 10:20:35 -0400

    more testing

--- src/os_x/test.sh
+++ src/os_x/test.sh
@@ -7,23 +7,23 @@ MATLAB9.4.app
 _9.4
 )
 
-printf "%s\n" ${MATLAB_9_4[0]}
-printf "%s\n" ${MATLAB_9_4[1]}
-printf "%s\n" ${MATLAB_9_4[2]}
-printf "%s\n" ${MATLAB_9_4[3]}
+#printf "%s\n" ${MATLAB_9_4[0]}
+#printf "%s\n" ${MATLAB_9_4[1]}
+#printf "%s\n" ${MATLAB_9_4[2]}
+#printf "%s\n" ${MATLAB_9_4[3]}
 
 get_matlab () {
-  printf "%s\\n" "RETRIEVING $1 INSTALLER..."
+  printf "%s\\n" "RETRIEVING ${MATLAB_9_4[0]} INSTALLER..."
 
-  #curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "$2" --output /Applications/matlab.app.tgz
+  curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "${MATLAB_9_4[1]}" --output /Applications/matlab.app.tgz
 }
 
 launch_matlab () {
-  printf "%s\\n" "LAUNCHING '$1'..."
+  printf "%s\\n" "LAUNCHING '${MATLAB_9_4[0]}'..."
 
   # this doesn't work!!! need to fix <--
 
-  printf "%s\n" matlab'$3'
+  printf "%s\n" matlab'${MATLAB_9_4[3]}'
 }
 
 get_matlab
