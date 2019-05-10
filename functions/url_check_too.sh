32ee322280aa612cc44de4f23497e9298e8181c6
marshki <marshall@morethantomorrow.org>
2019-05-10 19:05:30 -0400

    fixed!

--- functions/url_check_too.sh
+++ functions/url_check_too.sh
@@ -4,44 +4,31 @@
 #### Test around header response code, e.g.: #### 
 
 # GOOD: 
-# wget --spider --server-response https://geekberg.info 2>&1 | awk '/^  HTTP/{print $2}'|head -1 
+# wget --spider --server-response https://geekberg.info 2>&1 | awk '/HTTP\/1.1/{print $2}'|head -1 
 # 200
 
 # BAD: 
 # wget --spider --server-response http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz 2>&1 \ 
-# | awk '/^  HTTP/{print $2}' |head -1
+# awk '/HTTP\/1.1/{print $2}' |head -1
 # 302
 
 #url="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz"
-#url="https://www.google.com" 
-url="https://geekberg.info" 
+url="https://www.google.com" 
+#url="https://geekberg.info" 
 #url="https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg" 
 
 wget_check() { 
   # wget to check HTTP status code; error message if it fails
   
-  status_code=$(wget --spider --server-response $url 2>&1 | awk '/^ HTTP/{print $2}' | head -1)  
+  status_code=$(wget --spider --server-response $url 2>&1 | awk '/HTTP\/1.1/{print $2}' | head -1)  
 
   printf "%s\\n" "$status_code"
 
-  #if [ "$status_code" -ne "200" ] ; then 
-  #  printf "%s\\n" "BAD URL" 
-  #else 
-  #  printf "%s\\n" "GOOD URL" 
-#fi 
+  if [ "$status_code" -ne "200" ] ; then 
+    printf "%s\\n" "BAD URL" 
+  else 
+    printf "%s\\n" "GOOD URL" 
+fi 
 } 
 
 wget_check
-
-#curl_check(){ 
-  # curl to check HTTP status code; error message if it fails  
-
-  #status_code=$(curl --output /dev/null --silent --head --write-out '%{http_code}\n' $url)
-
-  #if [ $status_code -ne "200" ] ; then 
-    #printf "%s\\n" "BAD URL" 
-  #else 
-    #printf "%s\\n" "GOOD URL" 
-#fi 
-#} 
-#curl_check 
