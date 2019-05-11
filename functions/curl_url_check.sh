089e852bd2747337a1c7507132d0a52b4b900806
marshki <marshall@morethantomorrow.org>
2019-05-11 09:22:47 -0400

    rename

---
+++ functions/curl_url_check.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash 
+# quick and dirty HTTP status code of URL  
+
+#### Test around header response code, e.g.: #### 
+
+# GOOD: 
+# curl -Is http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz|head -n 1
+# HTTP/1.1 200 OK
+
+# BAD: 
+# curl -Is http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz|head -n 1
+# HTTP/1.1 302 Found
+# curl -Is https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg|head -1 
+# HTTP/1.1 401 Authorization Required
+# curl -Is http://citi.net |head -n 1
+# HTTP/1.1 301 Moved Permanently
+
+
+#url="http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz"
+#url="https://www.google.com" 
+url="https://geekberg.info" 
+#url="https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg" 
+
+url_check(){ 
+  # curl to check HTTP status code; error message if it fails  
+
+  status_code=$(curl --output /dev/null --silent --head --write-out '%{http_code}\n' $url)
+
+  if [ $status_code -ne "200" ] ; then 
+    printf "%s\\n" "BAD URL" 
+  else 
+    printf "%s\\n" "GOOD URL" 
+fi 
+} 
+
+url_check 
