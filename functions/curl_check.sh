658a6f44a73d1d45539bb8be8bf99b8f28f405bb
marshki <marshall@morethantomorrow.org>
2019-05-12 10:08:31 -0400

    describe function

--- functions/curl_check.sh
+++ functions/curl_check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 # Install curl if not present (Debian-based OSs) 
 
+# Query dpkg to get status of package, if count equals zero, install via apt
+
 curl_check () {
   if [ "$(dpkg-query --show --showformat='${Status}' curl 2>/dev/null | grep --count "ok installed")" -eq "0" ]; then
     printf "%s\\n" "CURL IS NOT INSTALLED. LET'S INSTALL IT..."
