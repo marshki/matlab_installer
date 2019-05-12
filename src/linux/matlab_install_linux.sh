b35571772ea16c7b43f7502d65710291dc61b364
marshki <marshall@morethantomorrow.org>
2019-05-12 09:54:01 -0400

    replace curl with wget to retrieve tarball

--- src/linux/matlab_install_linux.sh
+++ src/linux/matlab_install_linux.sh
@@ -81,7 +81,7 @@ sanity_checks () {
 get_matlab () {
   printf "%s\\n" "RETRIEVING ${MATLAB[0]} INSTALLER..."
 
-  curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "${MATLAB[1]}" --output /usr/local/matlab.tgz
+  wget --progress=bar --tries=3 --wait=5 --continue "${MATLAB[1]}" --output-document=/usr/local/matlab.tgz
 }
 
 # Unpack tarball to /usr/local, which installs Matlab.  
