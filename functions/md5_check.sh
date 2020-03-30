c9c1d1d1c2cde8ab60b6a0f6b52ec8408de6052e
marshki <marshall@morethantomorrow.org>
2020-03-30 16:38:34 -0400

    possibility

--- functions/md5_check.sh
+++ functions/md5_check.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-# Place holder. Need a way to confirm that the downloaded package is complete. 
-# checksum via md5 & diff seems like one way to do this.
+# Place holder. 
+# Confirm that the destination file matches the source using checksum via md5.
+# For reference: https://stackoverflow.com/questions/17988090/what-are-the-differences-between-md5-binary-mode-and-text-mode
+
+file1=`md5 $1`
+file2=`md5 $2`
+
+if [ "$file1" = "$file2" ]
+then
+    printf "%s\\n" "Same."
+else
+    printf "%s\\n" "Different."
+fi		
