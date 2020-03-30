9befebb882564c947b8be6a2bba6d45e397eb75d
marshki <marshall@morethantomorrow.org>
2020-03-30 17:47:01 -0400

    shell check error fix

--- functions/md5_check.sh
+++ functions/md5_check.sh
@@ -4,8 +4,8 @@
 # Confirm that the destination file matches the source using checksum via md5.
 # For reference: https://stackoverflow.com/questions/17988090/what-are-the-differences-between-md5-binary-mode-and-text-mode
 
-file1=`md5 $1`
-file2=`md5 $2`
+file1=$(md5 "$1")
+file2=$(md5 "$2")
 
 if [ "$file1" = "$file2" ]
 then
