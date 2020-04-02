279964b81adf87b71694d7ea4ac63b0ae60e53f9
marshki <marshall@morethantomorrow.org>
2020-04-02 13:15:10 -0400

    update

--- functions/md5_check.sh
+++ functions/md5_check.sh
@@ -4,12 +4,21 @@
 # Confirm that the destination file matches the source using checksum via md5.
 # For reference: https://stackoverflow.com/questions/17988090/what-are-the-differences-between-md5-binary-mode-and-text-mode
 
-file1=$(md5 "$1")
-file2=$(md5 "$2")
+#file1=$(md5sum "$1")
+#file2=$(md5sum "$2")
 
-if [ "$file1" = "$file2" ]
-then
-    printf "%s\\n" "Same."
-else
-    printf "%s\\n" "Different."
-fi		
+source=$(md5sum "$1")
+destination=$(md5sum "$2")
+
+md5_check () { 
+  printf "%s\\n" "Comparing hashes..."
+
+  if [ "$source" = "$destination" ]
+    then
+      printf "%s\\n" "Same."
+  else
+      printf "%s\\n" "Different."
+fi	
+} 
+
+md5_check	
