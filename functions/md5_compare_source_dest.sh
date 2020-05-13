1b69669dadb0f09760e631e433e79ae420d4943b
marshki <marshall@morethantomorrow.org>
2020-05-13 15:36:05 -0400

    add macos and gnu/linux support

--- functions/md5_compare_source_dest.sh
+++ functions/md5_compare_source_dest.sh
@@ -6,30 +6,22 @@
 # Step 1:
 # The source file's md5 hash.
 
-# GNU/Linux
-#SOURCE_HASH="8e2dc3b7c652fe284ea556ea9c478297"
-
-# macOS
-SOURCE_HASH="74c11e623c4acce27a637d8d89b840d1"
+SOURCE_HASH="8e2dc3b7c652fe284ea556ea9c478297"
 
 # Step 2: 
 # Download source file. 
 
-# GNU/Linux
-#SOURCE_FILE="https://gist.githubusercontent.com/marshki/25306338cc74d38fa1f99ad7a3e90578/raw/c8acfd61c9c3ca142e03d376bbeafbc9f82cfc7e/nyu_hpc_sshfs.md"
-
-# macOS
-SOURCE_FILE="https://localweb.cns.nyu.edu/mac/MATLAB7.5.app.tgz"
+SOURCE_FILE="https://gist.githubusercontent.com/marshki/25306338cc74d38fa1f99ad7a3e90578/raw/c8acfd61c9c3ca142e03d376bbeafbc9f82cfc7e/nyu_hpc_sshfs.md"
 
 get_source_file () {
 
   printf "%s\\n" "Retrieving source file..."
 
   # GNU/Linux
-  #wget --progress=bar --tries=3 --wait=5 --continue $SOURCE_FILE --output-document=/usr/local/nyu_hpc_sshfs.md
+  wget --progress=bar --tries=3 --wait=5 --continue $SOURCE_FILE --output-document=/usr/local/nyu_hpc_sshfs.md
 
   # macOS
-  curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "$SOURCE_FILE" --output /Applications/matlab.app.tgz
+  #curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "$SOURCE_FILE" --output /usr/local/nyu_hpc.sshfd.md
 }
 
 # Step 3: 
@@ -40,10 +32,10 @@ get_destination_hash () {
   printf "%s\\n" "Calculating hash..."
 
   # GNU/Linux
-  #DESTINATION_HASH="$(md5sum /usr/local/nyu_hpc_sshfs.md |awk '{print $1}')"
+  DESTINATION_HASH="$(md5sum /usr/local/nyu_hpc_sshfs.md |awk '{print $1}')"
 
   # macOS
-  DESTINATION_HASH="$(md5 -r /Applications/matlab.app.tgz |awk '{print $1}')"
+  #DESTINATION_HASH="$(md5 -r /usr/local/nyu_hpc_sshfs.md |awk '{print $1}')"
 }
 
 # Step 4: 
