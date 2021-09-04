ec0c341bad1344da84723da56b03f4791d49483b
marshki <marshall@morethantomorrow.org>
2021-09-04 08:58:21 -0400

    rm extra \ for printf

--- functions/wget_url_check.sh
+++ functions/wget_url_check.sh
@@ -20,12 +20,12 @@ wget_check() {
     
   status_code=$(wget --spider --server-response $url 2>&1 | awk '/HTTP\/1.1/{print $2}' | head -1)  
 
-  printf "%s\\n" "$status_code"
+  printf "%s\n" "$status_code"
 
   if [ "$status_code" -ne "200" ] ; then 
-    printf "%s\\n" "BAD URL" 
+    printf "%s\n" "BAD URL" 
   else 
-    printf "%s\\n" "GOOD URL" 
+    printf "%s\n" "GOOD URL" 
 fi 
 } 
 
