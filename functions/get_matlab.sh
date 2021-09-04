23cb27e57008b2fbf39b88946dbf08e3e900a5d8
marshki <marshall@morethantomorrow.org>
2021-09-04 08:30:57 -0400

    update shebang

--- functions/get_matlab.sh
+++ functions/get_matlab.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-# Retrieve tarball via curl or wget
+#!/usr/bin/env bash
+# Retrieve tarball via curl (or wget).
  
-# Download tarball to /usr/local. 
-# Show progress bar -> retry 3 times -> wait 5 seconds between retries 
-# (curl) keep connections alive for 60 secs [wget does this be default]
-# output download to /usr/local as "matlab.tgz"
-
+# Download tarball to /usr/local.
+# Show progress bar -> retry 3 times -> wait 5 seconds between retries. 
+# (curl) keep connections alive for 60 secs [wget does this be default].
+# output download to /usr/local as "matlab.tgz".
 
 MATLAB=(
 Matlab9.5
@@ -15,7 +13,7 @@ matlab9.5
 )
 
 get_matlab () {
-  printf "%s\\n" "RETRIEVING ${MATLAB[0]} INSTALLER..."
+  printf "%s\n" "RETRIEVING ${MATLAB[0]} INSTALLER..."
 
   #wget --progress=bar --tries=3 --wait=5 --continue "${MATLAB[1]}" --output-document=/usr/local/matlab.tgz 
   curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "${MATLAB[1]}" --output /usr/local/matlab.tgz
