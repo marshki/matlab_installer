1d5c872ea958a0ff8dd0c848365a1801b16fbe96
marshki <marshall@morethantomorrow.org>
2021-09-04 08:23:28 -0400

    add yes to apt; fix spacing

--- functions/curl_check.sh
+++ functions/curl_check.sh
@@ -1,12 +1,15 @@
-#!/bin/bash 
+#!/usr/bin/env bash 
 # Install curl if not present (Debian-based OSs) 
 
-# Query dpkg to get status of package, if count equals zero, install via apt
+# Query dpkg as to status of package. If count equals zero (0), install via apt.
 
 curl_check () {
-  if [ "$(dpkg-query --show --showformat='${Status}' curl 2>/dev/null | grep --count "ok installed")" -eq "0" ]; then
+
+  if [ "$(dpkg-query --show --showformat='${Status}' curl 2>/dev/null \
+    | grep --count "ok installed")" -eq "0" ]; then
+
     printf "%s\\n" "CURL IS NOT INSTALLED. LET'S INSTALL IT..."
-    apt-get install curl
+    apt-get install --yes curl
 fi
 }
 
