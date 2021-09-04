4a4e6e74a35b164aa96ac778739c1ee65cba058f
marshki <marshall@morethantomorrow.org>
2021-09-04 08:28:19 -0400

    fix fo 80 character limit

--- functions/disk_space_check.sh
+++ functions/disk_space_check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# available disk space check   
+# Available disk space check.
 
 # df on locally-mounted file systems in kbs | get 2nd line, 4th field 
 # less than or equal to " "
@@ -7,7 +7,8 @@
 check_disk_space () {
   
   if [ "$(df -lk /Applications |awk 'FNR == 2 {print $4}')" -le "7717519" ]; then
-    printf "%s\\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
+
+    printf "%s\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
     exit 1
 fi
 }
