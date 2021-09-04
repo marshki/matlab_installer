527f874db287366500a45f72435a47edd2c21b0a
marshki <marshall@morethantomorrow.org>
2021-09-04 08:35:30 -0400

    UPPER to lower for variable names

--- functions/md5_check.sh
+++ functions/md5_check.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # Compare md5 hashes
 
-SOURCE_HASH="8e2dc3b7c652fe284ea556ea9c478297"
-DESTINATION_HASH="8e2dc3b7c652fe284ea556ea9c478297"
+source_hash="8e2dc3b7c652fe284ea556ea9c478297"
+destination_hash="8e2dc3b7c652fe284ea556ea9c478297"
 
 md5_check () { 
-  printf "%s\\n" "Comparing hashes..."
-  printf "%s\\n" "$SOURCE_HASH"
-  printf "%s\\n" "$DESTINATION_HASH"
+  printf "%s\n" "Comparing hashes..."
+  printf "%s\n" "$source_hash"
+  printf "%s\n" "$destination_hash"
 
-  if [ "$SOURCE_HASH" = "$DESTINATION_HASH" ]
+  if [ "$source_hash" = "$destination_hash" ]
     then
-      printf "%s\\n" "Same."
+      printf "%s\n" "Same."
   else
-      printf "%s\\n" "Different."
+      printf "%s\n" "Different."
 fi	
 } 
 
