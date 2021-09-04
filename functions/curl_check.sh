c0496f554f55d440c58b44e8d1fea15ec014e99b
marshki <marshall@morethantomorrow.org>
2021-09-04 08:25:11 -0400

    rm extra \ from printf

--- functions/curl_check.sh
+++ functions/curl_check.sh
@@ -8,7 +8,7 @@ curl_check () {
   if [ "$(dpkg-query --show --showformat='${Status}' curl 2>/dev/null \
     | grep --count "ok installed")" -eq "0" ]; then
 
-    printf "%s\\n" "CURL IS NOT INSTALLED. LET'S INSTALL IT..."
+    printf "%s\n" "CURL IS NOT INSTALLED. LET'S INSTALL IT..."
     apt-get install --yes curl
 fi
 }
