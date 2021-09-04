eecbd509578795636d3c3d19e5482c9c9728be7d
marshki <marshall@morethantomorrow.org>
2021-09-04 08:44:15 -0400

    fix apt-get syntax (package --yes)

--- functions/pv_check.sh
+++ functions/pv_check.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env bash 
-# Is pv installed? If not, install it
-# add silent install so it doesn't break the dialog box
+#!/usr/bin/env bash
+
+# Is pv installed? If not, install it.
+# Add silent install so it doesn't break the dialog box.
 
 script=$(basename "$0")
 program="MATLAB INSTALLER"
 
 pv_check () {
+
   if [ "$(dpkg-query --show --showformat='${Status}' pv 2>/dev/null | grep --count "ok installed")" -eq "0" ]; then
     dialog --backtitle "$script" --title "$program" --infobox "PV IS NOT INSTALLED. LET'S INSTALL IT..." >&2 10 40
-    apt-get install pv --yes 
+    apt-get install --yes pv
 fi
 }
 
