702c3fdebcacef57e6d339300d9fc09b5debe850
marshki <marshall@morethantomorrow.org>
2022-07-06 12:19:32 -0400

    clean up comments for brevity

--- functions/curl_url_check.sh
+++ functions/curl_url_check.sh
@@ -1,34 +1,33 @@
-#!/usr/bin/env bash 
-# quick and dirty HTTP status code of URL  
+#!/usr/bin/env bash
+# A quick and dirty test of a URL's HTTP status code via `curl`.
 
-#### Test around header response code, e.g.: #### 
+# Sample URLs + header response codes:
 
-# GOOD: 
-# curl -Is www.cns.nyu.edu/mac/matlab9.11.tgz|head -n 1
-# HTTP/1.1 200 OK
+# GOOD:
+#  - curl -Is www.cns.nyu.edu/mac/matlab9.11.tgz|head -n 1
+#  - HTTP/1.1 200 OK
 
 # BAD: 
-# curl -Is http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz|head -n 1
-# HTTP/1.1 302 Found
-# curl -Is https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg|head -1 
-# HTTP/1.1 401 Authorization Required
-# curl -Is http://citi.net |head -n 1
-# HTTP/1.1 301 Moved Permanently
+# - curl -Is http://localweb.cns.nyu.edu/unixadmin/mat-distro-12-2014/macos/current-MATLAB.app.tgz|head -n 1
+# - HTTP/1.1 302 Found
+#
+# - curl -Is https://www.nyu.edu/its/software/vpn/anyconnect-macos-4.4.00243-predeploy-k9.dmg|head -1
+# - HTTP/1.1 401 Authorization Required
+# 
+# - curl -Is http://citi.net |head -n 1
+# - HTTP/1.1 301 Moved Permanently
 
-# url="https://geekberg.info" 
 url="www.cns.nyu.edu/mac/matlab9.11.tgz"
 
-# curl to check HTTP status code; error message if it fails  
-
-url_check(){ 
+url_check(){
   
   status_code=$(curl --output /dev/null --silent --head --write-out '%{http_code}\n' $url)
 
-  if [ $status_code -ne "200" ] ; then 
-    printf "%s\n" "BAD URL" 
-  else 
-    printf "%s\n" "GOOD URL" 
-fi 
-} 
+  if [ $status_code -ne "200" ] ; then
+    printf "%s\n" "BAD URL"
+  else
+    printf "%s\n" "GOOD URL"
+fi
+}
 
-url_check 
+url_check
