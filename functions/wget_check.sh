b4d07d13b815a090d0a6b7bfc338c9ef17510df6
marshki <marshall@morethantomorrow.org>
2022-07-06 16:38:51 -0400

    update docstring

--- functions/wget_check.sh
+++ functions/wget_check.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
-# Install wget if not present (Debian-based OSs) 
+#!/usr/bin/env bash 
+# Install `wget` if not present (Debian-based OSs).
 
-# Query dpkg to get status of package, if count equals zero, install via apt
+# Query `dpkg` to get status. If count equals zero (0), install via apt.
  
 wget_check () {
   if [ "$(dpkg-query --show --showformat='${Status}' wget 2>/dev/null | grep --count "ok installed")" -eq "0" ]; then
