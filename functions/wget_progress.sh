c7a78f447def1ce63a972427a39b027fc99c55e1
marshki <marshall@morethantomorrow.org>
2022-07-06 17:03:19 -0400

    cleanup docstring

--- functions/wget_progress.sh
+++ functions/wget_progress.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
-# Download tarball to /usr/local. 
-# Progress bar built off of gist from: https://gist.github.com/Gregsen/7822421
+# Progress bar for downloading tarball to: /usr/local. 
+# - built off of gist from: https://gist.github.com/Gregsen/7822421.
 
 MATLAB=(
 Matlab9.11
@@ -14,7 +13,7 @@ get_matlab () {
   wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue ${MATLAB[1]} 2>&1 | \
     grep "%" |\
     sed -u -e "s,\.,,g" | awk '{print $2}' | sed -u -e "s,\%,,g" |
-    dialog --backtitle "$script" --title "$program" --gauge "RETRIEVING ${MATLAB[0]} INSTALLER..." 10 40 
+    dialog --backtitle "$script" --title "$program" --gauge "RETRIEVING ${MATLAB[0]} INSTALLER..." 10 40
 }
 
 get_matlab
