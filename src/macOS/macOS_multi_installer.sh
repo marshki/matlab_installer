25758df1120cf9438bc871b93a4a7fe08a9fb730
marshki <marshall@morethantomorrow.org>
2022-11-15 12:33:20 -0500

    update URLS from web to localweb

--- src/macOS/macOS_multi_installer.sh
+++ src/macOS/macOS_multi_installer.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
-# macOS_multi_installer
+# sysOS_multi_installer
 #
-# Menu-based installer for pre-packaged versions of MATLAB on macOs.
+# Menu-based installer for pre-packaged versions of MATLAB on sysOs.
 # For use by NYU's: Center for Brain Imaging, Center for Neural Science,
 # and Department of Psychology.
 # Note: Does not add symoblic link to MATLAB binary.
@@ -25,70 +25,70 @@ local_web="http://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.11.a
 
 MATLAB_9_0=(
 Matlab9.0
-"https://localweb.cns.nyu.edu/mac/MATLAB9.0.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.0.app.tgz"
 MATLAB9.0.app
 _9.0
 )
 
 MATLAB_9_1=(
 Matlab9.1
-"https://localweb.cns.nyu.edu/mac/MATLAB9.1.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.1.app.tgz"
 MATLAB9.1.app
 _9.1
 )
 
 MATLAB_9_2=(
 Matlab9.2
-"https://localweb.cns.nyu.edu/mac/MATLAB9.2.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.2.app.tgz"
 MATLAB9.2.app
 _9.2
 )
 
 MATLAB_9_3=(
 Matlab9.3
-"https://localweb.cns.nyu.edu/mac/MATLAB9.3.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.3.app.tgz"
 MATLAB9.3.app
 _9.3
 )
 
 MATLAB_9_4=(
 Matlab9.4
-"https://localweb.cns.nyu.edu/mac/MATLAB9.4.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.4.app.tgz"
 MATLAB9.4.app
 _9.4
 )
 
 MATLAB_9_5=(
 Matlab9.5
-"https://localweb.cns.nyu.edu/mac/MATLAB9.5.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.5.app.tgz"
 MATLAB9.5.app
 _9.5
 )
 
 MATLAB_9_6=(
 Matlab9.6
-"https://localweb.cns.nyu.edu/mac/MATLAB9.6.app.tgz"
+"https://localweb.cns.nyu.edu/sys/macos/mat-archive-8-2016/MATLAB9.6.app.tgz"
 MATLAB9.6.app
 _9.6
 )
 
 MATLAB_9_7=(
 Matlab9.7
-"https://localweb.cns.nyu.edu/mac/MATLAB9.7.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.7.app.tgz"
 MATLAB9.7.app
 _9.7
 )
 
 MATLAB_9_9=(
 Matlab9.9
-"https://localweb.cns.nyu.edu/mac/MATLAB9.9.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.9.app.tgz"
 MATLAB9.9.app
 _9.9
 )
 
 MATLAB_9_11=(
 Matlab9.11
-"https://localweb.cns.nyu.edu/mac/MATLAB9.11.app.tgz"
+"https://localweb.cns.nyu.edu/sys/mat-archive-8-2016/macos/MATLAB9.11.app.tgz"
 MATLAB9.11.app
 _9.11
 )
@@ -117,7 +117,7 @@ check_disk_space () {
 fi
 }
 
-# Is curl installed? If not, exit. (Curl ships with macOS, but let's check).
+# Is curl installed? If not, exit. (Curl ships with sysOS, but let's check).
 
 curl_check () {
 
