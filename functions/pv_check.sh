138993e9ed2dddca78b666d7d93aad19eee7d0d5
marshki <marshall@morethantomorrow.org>
2024-07-24 12:02:27 -0400

    refactor comparison; still not silent

--- functions/pv_check.sh
+++ functions/pv_check.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-# Is `pv` installed? If not, use silent install so we don't break `dialog` box.
+# Install `pv` if not present (Debian-based OSs)
+# Query `dpkg` for `pv`, installing if via apt if not available
+# Use silent install (--quiet/ -qq) so we don't break `dialog` box? 
 
 script=$(basename "$0")
 program="MATLAB INSTALLER"
 
 pv_check () {
+  if ! dpkg-query --show --showformat='${Status}' pv 2>/dev/null | grep --count "ok installed"; then
+    dialog --backtitle "$script" --title "$program" --infobox "PV is NOT installed. Let's install it..." >&2 10 40
 
-  if [ "$(dpkg-query --show --showformat='${Status}' pv 2>/dev/null | grep --count "ok installed")" -eq "0" ]; then
-    dialog --backtitle "$script" --title "$program" --infobox "PV IS NOT INSTALLED. LET'S INSTALL IT..." >&2 10 40
     apt-get install --yes pv
 fi
 }
