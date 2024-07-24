0e61a5fe891d496e4a364e8ff905a170210df0fc
marshki <marshall@morethantomorrow.org>
2024-07-24 10:40:39 -0400

    update

--- functions/disk_space_check.sh
+++ functions/disk_space_check.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Available disk space check.
+# df on locally-mounted file systems (kbs) | awk to get 2nd line, 1st field
+# available space  
 
-# df on locally-mounted file systems in kbs | get 2nd line, 4th field 
-# less than or equal to " "
+check_disk_space() {
+  local required_space=14680064
+  local available_space=$(df --local -k --output=avail /usr/local |awk 'FNR == 2 {print $1}')
 
-check_disk_space () {
-  
-  if [ "$(df -lk /Applications |awk 'FNR == 2 {print $4}')" -le "7717519" ]; then
-
-    printf "%s\n" "ERROR: NOT ENOUGH FREE DISK SPACE. EXITING." >&2
+  if [ "$available_space" -lt "$required_space" ]; then
+    printf "%s\n" "ERROR: Not enough free disk space. Exiting." >&2
     exit 1
 fi
 }
