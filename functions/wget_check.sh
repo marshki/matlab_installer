6643d6d6acf352a2fd2e7e220a7c72d088283198
marshki <marshall@morethantomorrow.org>
2024-07-24 11:23:40 -0400

    simplify comparison operation

--- functions/wget_check.sh
+++ functions/wget_check.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash 
-# Install `wget` if not present (Debian-based OSs).
-
-# Query `dpkg` to get status. If count equals zero (0), install via apt.
+#!/usr/bin/env bash
+# Install `wget` if not present (Debian-based OSs)
+# Query `dpkg` for `wget`, installing it via apt if not available
  
 wget_check () {
-  if [ "$(dpkg-query --show --showformat='${Status}' wget 2>/dev/null | grep --count "ok installed")" -eq "0" ]; then
-    printf "%s\\n" "WGET IS NOT INSTALLED. LET'S INSTALL IT..."
-    apt-get --yes install wget
+  if ! dpkg-query --show --showformat='${Status}' wget 2>/dev/null | grep --quiet "ok installed"; then
+    printf "%s\\n" "Wget is NOT installed. Let's install it..."
+
+    apt-get install --yes wget
 fi
 }
 
