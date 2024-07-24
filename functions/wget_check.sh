93a49edcb022251b4f1112b507968b4cf59a6892
marshki <marshall@morethantomorrow.org>
2024-07-24 13:41:23 -0400

    update comments

--- functions/wget_check.sh
+++ functions/wget_check.sh
@@ -2,8 +2,6 @@
 # Install `wget` if not present (Debian-based OSs)
 # Query `dpkg` for `wget`, installing it via apt if not available
 
-# Note to self: can `dialog` and `apt-get` coexist?
-
 wget_check () {
   if ! dpkg-query --show --showformat='${Status}' wget 2>/dev/null | grep --quiet "ok installed"; then
     printf "%s\\n" "Wget is NOT installed. Let's install it..."
