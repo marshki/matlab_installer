43f2f255ad926e0eafa8d85d7c06dcfc2b52375f
marshki <marshall@morethantomorrow.org>
2024-08-22 12:34:51 -0400

    refactor for column line length

--- functions/wget_check.sh
+++ functions/wget_check.sh
@@ -3,7 +3,8 @@
 # Query `dpkg` for `wget`, installing it via apt if not available
 
 wget_check () {
-  if ! dpkg-query --show --showformat='${Status}' wget 2>/dev/null | grep --quiet "ok installed"; then
+  if ! dpkg-query --show --showformat='${Status}' wget 2>/dev/null \
+  | grep --quiet "ok installed"; then
     printf "%s\\n" "Wget is NOT installed. Let's install it..."
 
     apt-get install --yes wget
