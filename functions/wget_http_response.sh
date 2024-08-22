c39f7510b22a4c32396eddef7cc61dfe2e82cd8e
marshki <marshall@morethantomorrow.org>
2024-08-22 16:44:59 -0400

    grr...

--- functions/wget_http_response.sh
+++ functions/wget_http_response.sh
@@ -7,8 +7,11 @@ program="MATLAB INSTALLER"
 
 local_web="https://localweb.cns.nyu.edu:443/sys/mat-archive-8-2016/macos/MATLAB9.11.app.tgz"
 
+# wget --spider -S "https://geekberg.info" 2>&1 | grep "HTTP/" | awk '{print $2}'
+
+
 local_web_check() {
-  local status_code=$(wget --spider --server-response "$local_web" 2>&1 | awk '/HTTP\/1.1/{print $2}' | head -1)
+  local status_code=$(wget --spider -server-response "$local_web" 2>&1 | awk '/HTTP\/1.1/{print $2}' | head -1)
 
   if [ "$status_code" -ne "200" ] ; then
     dialog --backtitle "$script" --title "$program" --infobox "Error: CNS local web is NOT reachable. Exiting." >&2 10 40
