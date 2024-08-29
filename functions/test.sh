5dd76a6c09234ef5cb1c5ff2316f24eda3a6eafb
marshki <marshall@morethantomorrow.org>
2024-08-29 17:17:26 -0400

    add test.sh as workaround

---
+++ functions/test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# The site serving up this URL uses a dated SSL/TSL certificate
+# which prevents checking HTTP response codes.
+# As a workaround, let's check the connection status instead.
+
+url="https://localweb.cns.nyu.edu:443/sys/mat-archive-8-2016/macos/MATLAB9.11.app.tgz"
+
+wget_check() {
+  # Capture the connection status
+  connection_status=$(wget --spider --server-response --no-check-certificate "$url" 2>&1 \
+  | awk '/:443.../ {print $5}')
+
+  if [ "$connection_status" = "connected." ]; then
+    printf "%s\n" "SERVER IS REACHABLE."
+  else
+    printf "%s\n" "ERROR: SERVER IS NOT REACHABLE."
+    exit 1
+  fi
+}
+
+wget_check
