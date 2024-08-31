19ad7ec23d280f04bee69bd223956546d9c50ab4
marshki <marshall@morethantomorrow.org>
2024-08-31 13:21:18 -0400

    fix spacing

--- functions/get_matlab.sh
+++ functions/get_matlab.sh
@@ -13,10 +13,11 @@ matlab9.11
 )
 
 get_matlab () {
-  printf "%s\n" "RETRIEVING ${MATLAB[0]} INSTALLER..."
+  printf "%s\n" "Retrieving ${MATLAB[0]} Installer..."
 
   #wget --progress=bar --tries=3 --wait=5 --continue "${MATLAB[1]}" --output-document=/usr/local/matlab.tgz 
-  curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "${MATLAB[1]}" --output /usr/local/matlab.tgz
+  curl --progress-bar --retry 3 --retry-delay 5 --keepalive-time 60 --continue-at - "${MATLAB[1]}" \
+  --output /usr/local/matlab.tgz
 }
 
 get_matlab
