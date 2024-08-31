21fd37bf6c9c23516ad7c5e86b5eecc744f66aca
marshki <marshall@morethantomorrow.org>
2024-08-31 13:33:30 -0400

    spacing fix

--- functions/wget_check.sh
+++ functions/wget_check.sh
@@ -2,7 +2,7 @@
 # Install `wget` if not present (Debian-based OSs)
 # Query `dpkg` for `wget`, installing it via apt if not available
 
-wget_check () {
+wget_check() {
   if ! dpkg-query --show --showformat='${Status}' wget 2>/dev/null \
   | grep --quiet "ok installed"; then
     printf "%s\\n" "Wget is NOT installed. Let's install it..."
