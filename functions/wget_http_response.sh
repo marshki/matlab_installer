d4cf08ecfe73ee51d9479128e43162d9fceb19ba
marshki <marshall@morethantomorrow.org>
2024-08-31 13:36:16 -0400

    fix spacing

--- functions/wget_http_response.sh
+++ functions/wget_http_response.sh
@@ -11,7 +11,8 @@ local_web="https://localweb.cns.nyu.edu:443/sys/mat-archive-8-2016/macos/MATLAB9
 
 
 local_web_check() {
-  local status_code=$(wget --spider -server-response "$local_web" 2>&1 | awk '/HTTP\/1.1/{print $2}' | head -1)
+  local status_code=$(wget --spider -server-response "$local_web" 2>&1 \
+  | awk '/HTTP\/1.1/{print $2}' | head -1)
 
   if [ "$status_code" -ne "200" ] ; then
     dialog --backtitle "$script" --title "$program" --infobox "Error: CNS local web is NOT reachable. Exiting." >&2 10 40
