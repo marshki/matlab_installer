fc94f7e40e4ab575a12de31d451b54fb7edf0df9
marshki <marshall@morethantomorrow.org>
2024-08-31 13:40:27 -0400

    spacing

--- functions/wget_progress.sh
+++ functions/wget_progress.sh
@@ -8,11 +8,10 @@ Matlab9.11
 MATLAB9.11.app
 )
 
-get_matlab () {
+get_matlab() {
 
   wget --progress=dot --output-document=/usr/local/matlab.tgz --tries=3 --continue ${MATLAB[1]} 2>&1 | \
-    grep "%" |\
-    sed -u -e "s,\.,,g" | awk '{print $2}' | sed -u -e "s,\%,,g" |
+    grep "%" | sed -u -e "s,\.,,g" | awk '{print $2}' | sed -u -e "s,\%,,g" |
     dialog --backtitle "$script" --title "$program" --gauge "RETRIEVING ${MATLAB[0]} INSTALLER..." 10 40
 }
 
