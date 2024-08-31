027dcc5b77b79cada0c4d74c5905cb6927e3d105
marshki <marshall@morethantomorrow.org>
2024-08-31 13:13:43 -0400

    fix spacing

--- functions/curl_check.sh
+++ functions/curl_check.sh
@@ -3,7 +3,8 @@
 # Query `dpkg` for `curl`, installing it via apt if not available
  
 curl_check () {
-  if ! dpkg-query --show --showformat='${Status}' curl 2>/dev/null | grep --quiet "ok installed"; then
+  if ! dpkg-query --show --showformat='${Status}' curl 2>/dev/null \
+  | grep --quiet "ok installed"; then
     printf "%s\\n" "Curl is NOT installed. Let's install it..."
 
     apt-get install --yes curl
